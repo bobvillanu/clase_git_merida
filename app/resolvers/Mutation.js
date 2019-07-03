e222eabb894961f470ed628bbe2fb1c2e904d203
Roberto Villanueva Cab <rvillanueva@imasd.com.mx>
2019-07-02 21:24:42 -0500

    sin mutation

--- app/resolvers/Mutation.js
+++
@@ -1,4 +0,0 @@
-const MutationCustomers=require('./Mutation/customers');
-const MutationAddress=require('./Mutation/address');
-const MutationProducts=require('./Mutation/products');
-const MutationProviders=require('./Mutation/providers');
